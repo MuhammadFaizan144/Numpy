d7ab67e3b6f73db60470dfccfe889dfa46359174
Fazo01 <fg7829098@gmail.com>
2025-10-06 21:39:27 -0700

    complete vectorization # np.ininf(array) 10.1000

---
+++ 8-Handling Missing Value/1-isnan.py
@@ -0,0 +1,6 @@
+# np.isnan(array)
+import numpy as np
+arr1=np.array([1,2,np.nan,4,np.nan,6])
+print(np.isnan(arr1))
+# not
+print(np.nan==np.nan)
---
+++ 8-Handling Missing Value/2-nan_to_nan.py
@@ -0,0 +1,6 @@
+# np.nan_to_bum(array,nan=value) default - 0
+import numpy as np
+arr=np.array([1,2,np.nan,4,np.nan,6])
+# cleaned_arr=np.nan_to_num(arr,nan=100)
+cleaned_arr=np.nan_to_num(arr)
+print(cleaned_arr)
