ded62836b394c32b97dd5167108e6a4d8cb4b196
Fazo01 <fg7829098@gmail.com>
2025-10-08 09:34:46 -0700

    Q1 of numpy complete

---
+++ 3-Indexing & Slicing/Practice of Access/Q1.py
@@ -0,0 +1,14 @@
+# Create a 1D array of numbers 10 to 20. Access:
+# the first element
+# the last element
+# element at index 4
+
+import numpy as np
+_1d_array=np.arange(10,21)
+print(_1d_array)
+_1st_array=np.array([0])
+last_array=np.array([-1])
+element_array=np.array([4])
+print(_1st_array)
+print(last_array)
+print(element_array)
