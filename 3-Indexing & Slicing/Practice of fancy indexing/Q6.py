beb1c5eb3f28bcdea3cd13db7eb2b3bd9c574242
Fazo01 <fg7829098@gmail.com>
2025-10-09 21:52:39 -0700

    Q6 complete

---
+++ 3-Indexing & Slicing/Practice of fancy indexing/Q6.py
@@ -0,0 +1,7 @@
+# 6. Create an array `arr = np.arange(10, 20)` and access elements at indices `[1, 3, 5, 7]`.
+
+import numpy as np
+arr=np.arange(10,20)
+element=arr[[1,3,5,7]]
+print(arr)
+print(element)
