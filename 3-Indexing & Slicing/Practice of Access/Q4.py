3c3d09f2e03a417140179ff5f3b8485d8996574f
Fazo01 <fg7829098@gmail.com>
2025-10-09 21:45:15 -0700

    Q4 complete

---
+++ 3-Indexing & Slicing/Practice of Access/Q4.py
@@ -0,0 +1,6 @@
+
+# 4. Access all elements of the 1st column of a 5×5 array.
+import numpy as np
+arr=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[17,18,19,20]])
+fouth=arr[0,:]
+print(fouth)
