be4be91917c1ce4bd561d7d631c70d051e58f287
Fazo01 <fg7829098@gmail.com>
2025-10-12 04:29:48 -0700

    Q22 complete

--- 4-Reshaping & Manipulation/Practice Flattening/Q22.py
+++ 4-Reshaping & Manipulation/Practice Flattening/Q22.py
@@ -1,3 +1,7 @@
 # 22. Flatten a 3D array of shape (2,3,4) and verify total number of elements.
 import numpy as np
-arr=np.array()
+arr=np.arange(24).reshape(2,3,4)
+flat=arr.flatten()
+print(arr.shape)
+print(flat)
+print(flat.size)
