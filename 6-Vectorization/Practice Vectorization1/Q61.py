dc7c4353d6a4e16af326bd7ed272513214fd54ec
Fazo01 <fg7829098@gmail.com>
2025-10-18 08:25:46 -0700

    Complete vectorization

--- 6-Vectorization/Practice Vectorization1/Q61.py
+++ 6-Vectorization/Practice Vectorization1/Q61.py
@@ -1,1 +1,7 @@
 # 61. Convert a 1D array into 2D row and column vector.
+import numpy as np
+arr=np.array([1,2,3,4,5,6])
+row_vec=arr[np.newaxis,:]
+col_vec=arr[:,np.newaxis]
+print(row_vec)
+print(col_vec)
--- 6-Vectorization/Practice Vectorization1/Q62.py
+++ 6-Vectorization/Practice Vectorization1/Q62.py
@@ -1,1 +1,6 @@
 # 62. Add a 1D bias vector to a 2D matrix.
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6]])
+bias=np.array([10,20,30])
+res=arr+bias
+print(res)
